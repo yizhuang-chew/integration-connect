94ec5f3d14c8dda7814a0255528cb6992bddab8c
Yi Zhuang, Chew <chewyizhuang@gmail.com>
2024-09-19 10:51:11 +0800

    Delete custom-object-event/build directory

--- custom-object-event/build/constants/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NOTIFICATION_TYPE_RESOURCE_CREATED = void 0;
-exports.NOTIFICATION_TYPE_RESOURCE_CREATED = 'ResourceCreated';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uc3RhbnRzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vc3JjL2NvbnN0YW50cy9jb25zdGFudHMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQWEsUUFBQSxrQ0FBa0MsR0FBRyxpQkFBaUIsQ0FBQyJ9
